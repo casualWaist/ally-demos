68e2230e227abbd757f5d25a471a138e03fd0187
Matthew <119624750+casualWaist@users.noreply.github.com>
2024-08-19 10:46:13 -0400

    added diamond

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,19 @@ export default function Home() {
         </div>
         <div className="flex flex-row justify-between items-center">
             <RedJewel width={500} height={500}/>
+            <Diamond width={500} height={500}/>
         </div>
         <div className="flex flex-row justify-between items-center">
             <PurpleJewel width={250} height={250}/>
             <YellowJewel width={250} height={250}/>
             <RedJewel width={250} height={250}/>
+            <Diamond width={250} height={250}/>
         </div>
         <div className="flex flex-row justify-between items-center">
             <PurpleJewel width={100} height={100}/>
             <YellowJewel width={100} height={100}/>
             <RedJewel width={100} height={100}/>
+            <Diamond width={100} height={100}/>
         </div>
     </main>
   );
@@ -55,6 +58,19 @@ function YellowJewel({width, height}: {width: number, height: number}) {
 function RedJewel({width, height}: {width: number, height: number}) {
     const {RiveComponent} = useRive({
         src: "/red_jewel_rotate_basic.riv",
+        autoplay: true,
+    });
+    return (
+        <div style={{width: width, height: height}}>
+            <RiveComponent/>
+        </div>
+    )
+
+}
+
+function Diamond({width, height}: {width: number, height: number}) {
+    const {RiveComponent} = useRive({
+        src: "/diamond_rotate_basic.riv",
         autoplay: true,
     });
     return (
