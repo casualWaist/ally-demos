b839e7cb8b9371b0d24b5d932d86926a6d38c611
Matthew <119624750+casualWaist@users.noreply.github.com>
2025-03-17 09:40:44 -0400

    best i got

--- public/fluid_sim.js
+++ public/fluid_sim.js
@@ -369,104 +369,6 @@ const gradientShader = compileShader(gl.FRAGMENT_SHADER, `
     uniform vec4 color6;
     uniform float time;
     
-    vec4 permute(vec4 x) {
-        return mod(((x*34.0)+1.0)*x, 289.0);
-    }
-    vec4 taylorInvSqrt(vec4 r) {
-        return 1.79284291400159 - 0.85373472095314 * r;
-    }
-    vec3 fade(vec3 t) {
-        return t*t*t*(t*(t*6.0-15.0)+10.0);
-    }
-    
-    mat2 rot(float a) {
-        float s = sin(a);
-        float c = cos(a);
-        return mat2(c, -s, s, c);
-    }
-    
-    
-    mat2 revRot(float a) {
-        float s = sin(a);
-        float c = cos(a);
-        return mat2(s, -c, c, s);
-    }
-    
-    float cnoise(vec3 P) {
-        vec3 Pi0 = floor(P); // Integer part for indexing
-        vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
-        Pi0 = mod(Pi0, 289.0);
-        Pi1 = mod(Pi1, 289.0);
-        vec3 Pf0 = fract(P); // Fractional part for interpolation
-        vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
-        vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
-        vec4 iy = vec4(Pi0.yy, Pi1.yy);
-        vec4 iz0 = Pi0.zzzz;
-        vec4 iz1 = Pi1.zzzz;
-    
-        vec4 ixy = permute(permute(ix) + iy);
-        vec4 ixy0 = permute(ixy + iz0);
-        vec4 ixy1 = permute(ixy + iz1);
-    
-        vec4 gx0 = ixy0 / 7.0;
-        vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
-        gx0 = fract(gx0);
-        vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
-        vec4 sz0 = step(gz0, vec4(0.0));
-        gx0 -= sz0 * (step(0.0, gx0) - 0.5);
-        gy0 -= sz0 * (step(0.0, gy0) - 0.5);
-    
-        vec4 gx1 = ixy1 / 7.0;
-        vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
-        gx1 = fract(gx1);
-        vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
-        vec4 sz1 = step(gz1, vec4(0.0));
-        gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-        gy1 -= sz1 * (step(0.0, gy1) - 0.5);
-    
-        vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
-        vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
-        vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
-        vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
-        vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
-        vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
-        vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
-        vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
-    
-        vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
-        g000 *= norm0.x;
-        g010 *= norm0.y;
-        g100 *= norm0.z;
-        g110 *= norm0.w;
-        vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
-        g001 *= norm1.x;
-        g011 *= norm1.y;
-        g101 *= norm1.z;
-        g111 *= norm1.w;
-    
-        float n000 = dot(g000, Pf0);
-        float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
-        float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
-        float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
-        float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
-        float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
-        float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
-        float n111 = dot(g111, Pf1);
-    
-        vec3 fade_xyz = fade(Pf0);
-        vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
-        vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-        float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
-        return 2.2 * n_xyz;
-    }
-    
-    float dss(vec2 p1, vec2 p2, vec2 uv) {
-        uv.x *= uv.y + uv.x;
-        float d1 = smoothstep(0.0, 1.0, distance(p1, uv));
-        float d2 = smoothstep(0.0, 0.5, distance(p2, uv));
-        return smoothstep(0.0, 1.0, distance(d1, d2));
-    }
-    
     vec4 screen(vec4 base, vec4 blend){
         return vec4(
             1.0 - ((1.0 - base.r) * (1.0 - blend.r)),
@@ -476,17 +378,23 @@ const gradientShader = compileShader(gl.FRAGMENT_SHADER, `
         );
     }
     
-    float blendColorBurn(float base, float blend){
-        return (blend==0.0)?blend:max((1.0-((1.0-base)/blend)),0.0);
+    mat2 rot(float a) {
+        float s = sin(a);
+        float c = cos(a);
+        return mat2(c, -s, s, c);
     }
     
-    vec4 blendColorBurn(vec4 base, vec4 blend){
-        return vec4(
-            blendColorBurn(base.r, blend.r),
-            blendColorBurn(base.g, blend.g),
-            blendColorBurn(base.b, blend.b),
-            1.0
-        );
+    mat2 revRot(float a) {
+        float s = sin(a);
+        float c = cos(a);
+        return mat2(s, -c, 0.5, s);
+    }
+    
+    float dss(vec2 p1, vec2 p2, vec2 uv) {
+        uv.x *= uv.y + uv.x;
+        float d1 = smoothstep(0.0, 1.0, distance(p1, uv));
+        float d2 = smoothstep(0.0, 0.5, distance(p2, uv));
+        return smoothstep(0.0, 1.0, distance(d1, d2));
     }
 
     void main() {
@@ -494,8 +402,8 @@ const gradientShader = compileShader(gl.FRAGMENT_SHADER, `
         vec2 ogUv = uv;
         uv -= 0.5;
         vec2 uv2 = uv;
-        uv *= rot(time * 0.025);
-        uv2 *= revRot(time * 0.037);
+        uv *= rot(time * 0.035);
+        uv2 *= revRot(time * 0.067);
         vec4 black = vec4(0.1, 0.0, 0.1, 1.0);
         vec4 red = mix(black, color6, dss(point1, point2, uv));
         vec4 blue = mix(black, color5, dss(point1 + 0.5, point2 + 0.5, uv));
@@ -503,20 +411,7 @@ const gradientShader = compileShader(gl.FRAGMENT_SHADER, `
         vec4 red2 = mix(black, color6, dss(point4, point5, uv2));
         vec4 blue2 = mix(black, color5, dss(point5 - 0.75, point6, uv2));
         vec4 green2 = mix(black, color2, dss(point4 - 1.0, point5, uv2));
-        vec4 blend1 = screen(red, green);
-        vec4 blend2 = screen(green, blue);
-        vec4 blend3 = screen(red2, blue);
-        vec4 blend4 = screen(red2, green2);
-        vec4 blend5 = screen(blue2, green2);
-        vec4 blend6 = screen(blue2, red);
-        float n = cnoise(blend5.rgb);
-        vec4 mix1 = mix(blend1, blend2, blend1.r * n);
-        vec4 mix2 = mix(blend2, blend3, blend2.g);
-        vec4 mix3 = mix(blend3, blend4, n);
-        vec4 mix4 = mix(blend4, blend5, n);
-        vec4 mix5 = mix(blend5, blend6, n);
-        //gl_FragColor = clamp(screen(max(mix1, mix2), mix3), 0.0, 0.9);
-        vec4 color = screen(screen(screen(red, blue), green), vec4(0.4, 0.4, 0.4, 1.0));
+        vec4 color = screen(screen(screen(red, blue), green), vec4(0.2, 0.4, 0.4, 1.0));
         gl_FragColor = screen(color, screen(screen(red2, blue2), green2));
     }
 `);
@@ -954,10 +849,10 @@ const targetPoints = [
 ];
 
 const currentPoints = [
-    { x: -0.5, y: 0.5 },
-    { x: -0.5, y: -0.5 },
-    { x: 0.5, y: 0.5 },
-    { x: 0.5, y: -0.5 },
+    { x: -0.25, y: 0.25 },
+    { x: -0.25, y: -0.25 },
+    { x: 0.25, y: 0.25 },
+    { x: 0.25, y: -0.25 },
     { x: -0.221, y: -1.21 },
     { x: -0.44, y: 0.243 }
 ];
@@ -966,7 +861,7 @@ update();
 
 function interpolatePoints(dt, diff) {
     const d = Math.min(diff, 0.999)
-    const interpolationSpeed = dt * d * 0.5;
+    const interpolationSpeed = dt * d * 0.6;
     for (let i = 0; i < currentPoints.length; i++) {
         currentPoints[i].x += (targetPoints[i].x - currentPoints[i].x) * interpolationSpeed;
         currentPoints[i].y += (targetPoints[i].y - currentPoints[i].y) * interpolationSpeed;
@@ -1268,9 +1163,9 @@ function correctDeltaY (delta) {
 
 function generateColor () {
     let c = HSVtoRGB(Math.random(), 1.0, 1.0);
-    c.r *= 0.1;
-    c.g *= 0.1;
-    c.b *= 0.1;
+    c.r *= 0.05;
+    c.g *= 0.05;
+    c.b *= 0.05;
     return c;
 }
 
